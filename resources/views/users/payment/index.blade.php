bddcb279c8707be86dcbb335d90f41db16a33364
Maykel <michael.eligoyo.flora@gmail.com>
2023-11-23 10:15:28 +0800

    change ui format contract form

--- resources/views/users/payment/index.blade.php
+++ resources/views/users/payment/index.blade.php
@@ -38,11 +38,77 @@
     </div>
 @endif
 <!-- Aler End -->
+<form id="contractForm" action="{{ route('authenticate.payment.store') }}" method="POST" autocomplete="off">@csrf
+    <input type="hidden" class="form-control" name="id">
+    <div class="card border mb-2">
+        <div class="card-body py-2 ">
+        <div class="row">
+            <div class="col-4">
+                <div class="form-group mb-1">
+                    <small for="">Supplier</small>
+                    <select id="{{ route("authenticate.report.filter.supplier") }}" name="suppliername" class="form-control"></select>
+                </div>
+                <div class="form-group mb-1">
+                    <small for="">Contract Number</small>
+                    <input type="text" class="needFormat form-control form-control-sm" required name="reference" maxlength="100">
+                </div>
+                <div class="form-group mb-1">
+                    <small for="">Item Description</small>
+                    <select id="{{ route("authenticate.report.filter.description") }}" class="form-control form-control-sm" required name="description"></select>
+                </div>
+            </div>
+            <div class="col-4">
+                    <div class="form-group mb-1">
+                        <small for="">Total Metric Ton</small>
+                        <input type="text" class="needFormat form-control form-control-sm amount-class" required name="metricTon" maxlength="15">
+                    </div>
+                    <div class="form-group mb-0">
+                        <small for="">Total Price MT (USD)</small>
+                        <input type="text" class="needFormat form-control form-control-sm amount-class" name="priceMetricTon" maxlength="15">
+                    </div>
+                    <div class="form-group mb-1">
+                        <small for="">&nbsp;</small>
+                        <div class="input-group input-group-sm">
+                            <div class="input-group-prepend">
+                              <span class="input-group-text" id="inputGroup-sizing-sm" style="font-size:10px">Total Amount (USD)</span>
+                            </div>
+                            <input type="text" class="form-control form-control-sm amount-class" required name="amountUSD" maxlength="20" readonly>
+                        </div>
+                    </div>
+            </div>
+            <div class="col-4">
+                <div class="card border mb-1 mt-2">
+                    <div class="card-header p-0 pl-2 font-bold"><small style="font-size:10px">ADVANCE PAYMENT</small></div>
+                    <div class="card-body p-2 pt-0">
+                        <div class="form-row pt-0">
+                            <div class="form-group col-12 mt-0 mb-1">
+                                <small for="">Intial Amount (USD)</small>
+                                <input type="text" class="needFormat form-control form-control-sm amount-class" required name="paidAmountUSD" maxlength="20">
+                            </div>
+                            <div class="form-group col-12 mb-1">
+                                <small for="">Percent</small>
+                                <input type="text" class="form-control form-control-sm" required name="contract_percent" maxlength="3">
+                            </div>
+                        </div>
+                    </div>
+                   </div>
+            </div>
+        </div>
+    </div>
+    <div class="card-footer p-1 ">
+        <span class="float-right">
+            <button type="submit" class="btn btn-sm btn-primary ">Submit</button>
+            <button type="button" class="btn btn-sm btn-warning " onclick="location.reload();">Cancel</button>
+        </span>
+    </div>
+   </div>
+</form>
 <div class="card">
     <div class="card-body p-3">
-        <div class="mt-4">
+        <div class="mt-1">
             <div class="row">
-                <div class="col-8">
+                <div class="col-12">
+                <div class="table-responsive">
                     <table id="contractTable" 
                     data-url="{{ route('authenticate.payment.list') }}"
                     data-remove="{{ route('authenticate.payment.delete',['cp']) }}"
@@ -64,60 +130,8 @@
                     </thead>
                 </table>
                 </div>
-                <div class="col-4">
-                    <form id="contractForm" action="{{ route('authenticate.payment.store') }}" method="POST" autocomplete="off">@csrf
-                            <input type="hidden" class="form-control" name="id">
-                            <div class="card border mb-2">
-                                <div class="card-body p-3">
-                                <div class="form-group">
-                                    <small for="">Supplier</small>
-                                    <select id="{{ route("authenticate.report.filter.supplier") }}" name="suppliername" class="form-control"></select>
-                                </div>
-                                <div class="form-group">
-                                    <small for="">Contract Number</small>
-                                    <input type="text" class="needFormat form-control form-control-sm" required name="reference" maxlength="100">
-                                </div>
-                                <div class="form-group">
-                                    <small for="">Item Description</small>
-                                    <select id="{{ route("authenticate.report.filter.description") }}" class="form-control form-control-sm" required name="description"></select>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-group col-6 mb-2">
-                                        <small for="">Total Metric Ton</small>
-                                        <input type="text" class="needFormat form-control form-control-sm amount-class" required name="metricTon" maxlength="15">
-                                    </div>
-                                    <div class="form-group col-6 mb-2">
-                                        <small for="">Total Price MT (USD)</small>
-                                        <input type="text" class="needFormat form-control form-control-sm amount-class" name="priceMetricTon" maxlength="15">
-                                    </div>
-                                </div>
-                                <div class="input-group input-group-sm mb-0">
-                                    <div class="input-group-prepend">
-                                      <span class="input-group-text" id="inputGroup-sizing-sm" style="font-size:10px">Total Amount (USD)</span>
-                                    </div>
-                                    <input type="text" class="form-control form-control-sm amount-class" required name="amountUSD" maxlength="20" readonly>
-                                </div>
-                            </div>
-                           </div>
-                           <div class="card border mb-1">
-                            <div class="card-header p-0 pl-2 font-bold"><small style="font-size:10px">ADVANCE PAYMENT</small></div>
-                            <div class="card-body p-3">
-                                <div class="form-row">
-                                    <div class="form-group col-6 mb-1">
-                                        <small for="">Intial Amount (USD)</small>
-                                        <input type="text" class="needFormat form-control form-control-sm amount-class" required name="paidAmountUSD" maxlength="20">
-                                    </div>
-                                    <div class="form-group col-6 mb-1">
-                                        <small for="">Percent</small>
-                                        <input type="text" class="form-control form-control-sm" required name="contract_percent" maxlength="3">
-                                    </div>
-                                </div>
-                            </div>
-                           </div>
-                            <button type="submit" class="btn btn-sm btn-primary btn-block">Submit</button>
-                            <button type="button" class="btn btn-sm btn-warning btn-block" onclick="location.reload();">Cancel</button>
-                    </form>
                 </div>
+               
             </div>
         </div>
     </div>
