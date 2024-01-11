861d728309ee9f050fb4d82b973cd2b48a0a596b
Maykel <michael.eligoyo.flora@gmail.com>
2024-01-11 11:31:28 +0800

    sorting supplier and item

--- resources/views/users/reports/preview/projectedCostList.blade.php
+++ resources/views/users/reports/preview/projectedCostList.blade.php
@@ -87,95 +87,99 @@
         $arr = [];
         $coll = collect($data);
         $res  = $coll->groupBy('description');
-        $key  = collect($data)->unique(['description'])->pluck('description');   
-    
-        function sorted($a, $b) {
-            return $a['suppliername'] - $b['suppliername'];
-        }
+        $key  = collect($data)->sortBy('description')->unique(['description'])->pluck('description');
        
         for($i=0; $i <count($key) ; $i++){
-            if ( $key[$i]!='EMPTY SACK') {
+            if ($key[$i]!='EMPTY SACK') {
                 $arr[]=[
                     'result'=> $res[$key[$i]],
                     'name'  => $key[$i]
                 ];
             }
         }
-
+        $tmp = array();
     @endphp
     <div class="accordion" id="accordionExample">
     @foreach ($arr as $key =>  $item)
+    @php
+        foreach($item['result']->sortBy('description')  as $arg){
+            $tmp[$arg->suppliername][] = $arg;
+        }
+    @endphp
 
         <div class="card">
           <div class="card-header p-0 bg-dark" id="headingOne-{{ $key }}">
             <h2 class="mb-0">
-              <button class="btn btn-block text-left text-white" style="font-size: 11px" type="button" data-toggle="collapse" data-target="#collapseOne-{{ $key }}" aria-controls="collapseOne-{{ $key }}">
+              <button class="btn btn-block text-left text-white" style="font-size: 11px" type="button" data-toggle="collapse" data-target="#collapseOne-{{ $key }}" @if ($key==0) aria-expanded="true" @endif  aria-controls="collapseOne-{{ $key }}">
                 {{ ++$c.'. '.$item['name'] }}
               </button>
             </h2>
           </div>
       
-          <div id="collapseOne-{{ $key }}" class="collapse" aria-labelledby="headingOne-{{ $key }}" data-parent="#accordionExample">
+          <div id="collapseOne-{{ $key }}" class="collapse @if ($key==0) show active @endif " aria-labelledby="headingOne-{{ $key }}" data-parent="#accordionExample">
             <div class="card-body">
-                <table id="" class="table table-sm table-striped table-bordered" style="font-size: 10px">
-                    <tr class="bg-secondary text-white" style="border:1px solid black">
-                        <th>suppliername</th>
-                        <th>Invoice</th>
-                        <th>Description</th>
-                        <th>AVG ExRate (PHP)</th>
-                        <th>Qty(Kls) </th>
-                        @foreach ($particulars as $itempar)
-                            <th>{{ $itempar->p_name }}</th>
-                        @endforeach
-                        <th>Projected Cost</th>
-                    </tr>
-                    @php
-                        $sort = $item['result']->sortBy('suppliername');
-
-                    @endphp
-                    @foreach($sort as $val)
-                    <tr>
-                        <td class="">{{ $val->suppliername }}</td>
-                        <td class="">{{ $val->invoiceno }}</td>
-                        <td class="">{{ $val->description }}</td>
-                        @php
-                            $projectedCost=0;
-                            $avgRes = DB::select("select  b.amount, d.exchangeRate from details a 
-                                                        inner join landedcost_particulars b on b.detail_id = a.id
-                                                        inner join lcdpnegos d on d.landedcost_particular_id=b.id
-                                                        where a.id={$val->id}");
-                            $avgInPHP = (array_sum(array_column($avgRes,'exchangeRate')));
-                        @endphp
-                        <th>
-                            {{ number_format((empty($avgInPHP)?'':($avgInPHP/count($avgRes))),4) }}
-                        </th>
-                        <th class="">{{ $val->qtykls }}</th>
-                        @php
-                            $projectedCost=0;
-                            $datapart = DB::select("select b.amount from details a 
-                                                inner join landedcost_particulars b on b.detail_id = a.id
-                                                inner join particulars c on b.particular_id = c.id  where a.id={$val->id}");
-                             $projectedCost = array_sum(array_column($datapart,'amount'));
-                        @endphp
-                        @foreach ($datapart as $i => $value)
-                            <td>
-                                {{ empty($value->amount)?'': number_format($value->amount,4) }}
-                            </td>
-                        @endforeach
-                        <th>
+                @foreach ($tmp as $k=> $dataVal)
+                    <table id="" class="table table-sm table-striped table-bordered" style="font-size: 10px">
+                        <tr class="bg-secondary text-white" style="border:1px solid black">
+                            <th>Suppliername</th>
+                            <th>Invoice</th>
+                            <th>Description</th>
+                            <th>AVG ExRate (PHP)</th>
+                            <th>Qty(Kls) </th>
+                            @foreach ($particulars as $itempar)
+                                <th>{{ $itempar->p_name }}</th>
+                            @endforeach
+                            <th>Projected Cost</th>
+                        </tr>
+                        @foreach($dataVal as $val)
+                        <tr>
+                            <td class="">{{ $val->suppliername }}</td>
+                            <td class="">{{ $val->invoiceno }}</td>
+                            <td class="">{{ $val->description }}</td>
                             @php
-                                $res = ($projectedCost/$val->qtykls) ?? 0;
-                                echo number_format($res,4);
-                                $res=0;
+                                $projectedCost=0;
+                                $avgRes = DB::select("select  b.amount, d.exchangeRate from details a 
+                                                            inner join landedcost_particulars b on b.detail_id = a.id
+                                                            inner join lcdpnegos d on d.landedcost_particular_id=b.id
+                                                            where a.id={$val->id}");
+                                $avgInPHP = (array_sum(array_column($avgRes,'exchangeRate')));
                             @endphp
-                        </th>
-                    </tr>
-                  @endforeach
-                </table>
+                            <th>
+                                {{ number_format((empty($avgInPHP)?'':($avgInPHP/count($avgRes))),4) }}
+                            </th>
+                            <th class="">{{ $val->qtykls }}</th>
+                            @php
+                                $projectedCost=0;
+                                $datapart = DB::select("select b.amount from details a 
+                                                    inner join landedcost_particulars b on b.detail_id = a.id
+                                                    inner join particulars c on b.particular_id = c.id  where a.id={$val->id}");
+                                $projectedCost = array_sum(array_column($datapart,'amount'));
+                            @endphp
+                            @foreach ($datapart as $i => $value)
+                                <td>
+                                    {{ empty($value->amount)?'': number_format($value->amount,4) }}
+                                </td>
+                            @endforeach
+                            <th>
+                                @php
+                                    $res = ($projectedCost/$val->qtykls) ?? 0;
+                                    echo number_format($res,4);
+                                    $res=0;
+                                @endphp
+                            </th>
+                        </tr>
+                        @endforeach
+                    </table>
+                    @php
+                        $tmp=[];    
+                    @endphp
+                @endforeach
+               
             </div>
           </div>
         </div>
-        @endforeach
+    @endforeach
+      
     </div>
     @endif
    </div>
